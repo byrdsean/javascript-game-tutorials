a11a58ce1eb333b650ba840e7b4b2e56b003dd97
Sean Byrd <byrd.sean1015@gmail.com>
2023-04-30 23:03:26 -0400

    Created a class to contain the image data, and x/y positions. Refactored code to paint all images to canvas at varying speeds.

--- tutorial-2/script.js
+++ tutorial-2/script.js
@@ -7,31 +7,51 @@ const CANVAS_HEIGHT = 700;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
-//Create Image objs for each image we will work with
-const backgroundLayer1 = new Image();
-backgroundLayer1.src = "./images/layer-1.png";
-const backgroundLayer2 = new Image();
-backgroundLayer2.src = "./images/layer-2.png";
-const backgroundLayer3 = new Image();
-backgroundLayer3.src = "./images/layer-3.png";
-const backgroundLayer4 = new Image();
-backgroundLayer4.src = "./images/layer-4.png";
-const backgroundLayer5 = new Image();
-backgroundLayer5.src = "./images/layer-5.png";
-
 let gameSpeed = 15;
 const MAX_IMAGE_WIDTH = 2400; //Width of all the images
 
-//X position of image
-let x_position = 0;
+class Layer {
+  constructor(imagePath, speedModifier) {
+    this.layerImage = new Image();
+    this.layerImage.src = imagePath;
+
+    this.speedModifier = speedModifier;
+    this.x_position = 0;
+    this.y_position = 0;
+  }
+
+  updateXPosition() {
+    this.x_position =
+      this.x_position === MAX_IMAGE_WIDTH * -1
+        ? 0
+        : this.x_position - gameSpeed * this.speedModifier;
+  }
+}
+
+const background1 = new Layer("./images/layer-1.png", 0.125);
+const background2 = new Layer("./images/layer-2.png", 0.25);
+const background3 = new Layer("./images/layer-3.png", 0.5);
+const background4 = new Layer("./images/layer-4.png", 0.75);
+const background5 = new Layer("./images/layer-5.png", 1);
+
+const backgroundLayers = [
+  background1,
+  background2,
+  background3,
+  background4,
+  background5,
+];
 
 function animate() {
   //Clear the entire canvas each time this function is called
   //Clears everthing from coords 0,0 to CANVAS_WIDTH, CANVAS_HEIGHT
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(backgroundLayer4, x_position, 0);
-  ctx.drawImage(backgroundLayer4, x_position + MAX_IMAGE_WIDTH, 0);
-  x_position = x_position === MAX_IMAGE_WIDTH * -1 ? 0 : x_position - gameSpeed;
+
+  backgroundLayers.forEach((layer) => {
+    ctx.drawImage(layer.layerImage, layer.x_position, 0);
+    ctx.drawImage(layer.layerImage, layer.x_position + MAX_IMAGE_WIDTH, 0);
+    layer.updateXPosition();
+  });
   requestAnimationFrame(animate);
 }
 animate();
